5c337769aecebb6d299b1ae63bcd628e3e8bc2b8
FlaviusMedIng <flavius.medecin1ingenieur@gmail.com>
2026-03-16 20:15:51 +0100

    Refactor PiletRenaudScraper's parse_list_page method

--- scrapers/pilet_renaud.py
+++ scrapers/pilet_renaud.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from scrapers.base_scraper import BaseScraper
 from utils.parser import clean_text
 
@@ -7,19 +5,20 @@ from utils.parser import clean_text
 class PiletRenaudScraper(BaseScraper):
 
     name = "pilet_renaud"
+
     use_playwright = True
 
-    def parse_list_page(self, soup, base_url: str) -> list[dict]:
+    load_strategy = "domcontentloaded"
 
-        listings: list[dict] = []
+    urls = [
+        "https://www.pilet-renaud.ch/fr/accueil/search/transaction/rent"
+    ]
 
-        # sélecteurs plus fiables
-        cards = soup.select(
-            "div.property-card, div.search-result, article"
-        )
+    def parse_list_page(self, soup, base_url):
 
-        if not cards:
-            cards = soup.select("a[href*='/fr/location/']")
+        listings = []
+
+        cards = soup.select("div.property-card, div.search-result, article")
 
         for card in cards:
 
@@ -28,35 +27,19 @@ class PiletRenaudScraper(BaseScraper):
             if not link:
                 continue
 
-            url = self.absolutize(base_url, link.get("href"))
+            url = self.absolutize(base_url, link["href"])
 
-            text_blob = clean_text(card.get_text(" ", strip=True))
+            blob = clean_text(card.get_text(" ", strip=True))
 
-            title = clean_text(link.get_text(" ", strip=True))
-
-            if not title:
-                title = text_blob[:120]
-
-            # filtre moins agressif
-            keywords = [
-                "commercial",
-                "arcade",
-                "bureau",
-                "activité",
-                "local",
-                "surface"
-            ]
-
-            if not any(k in text_blob.lower() for k in keywords):
-                continue
+            title = clean_text(link.get_text()) or blob[:100]
 
             listings.append(
                 self.make_listing(
                     url=url,
                     title=title,
-                    text_blob=text_blob,
+                    text_blob=blob,
                     site=self.name,
-                    location_hint=text_blob
+                    location_hint=blob
                 )
             )
 
