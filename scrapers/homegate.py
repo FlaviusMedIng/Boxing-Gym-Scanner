4bf8f42f362920140ff8b2e4879d7c1656e8e9aa
FlaviusMedIng <flavius.medecin1ingenieur@gmail.com>
2026-03-16 20:57:08 +0100

    Update homegate.py

--- scrapers/homegate.py
+++ scrapers/homegate.py
@@ -5,27 +5,32 @@ from utils.parser import clean_text
 class HomegateScraper(BaseScraper):
 
     name = "homegate"
-
     use_playwright = True
-
     load_strategy = "domcontentloaded"
 
-    urls = [
-        "https://www.homegate.ch/rent/industrial-object/city-geneva/matching-list"
-    ]
+    def __init__(self, site_cfg, config, logger):
+        super().__init__(site_cfg, config, logger)
+
+        self.urls = [
+            "https://www.homegate.ch/rent/industrial-object/city-geneva/matching-list"
+        ]
 
     def parse_list_page(self, soup, base_url):
 
         listings = []
 
-        cards = soup.select("a[href*='/rent/']")
+        cards = soup.select("a[data-testid='listing-link']")
 
         for link in cards:
 
-            url = self.absolutize(base_url, link.get("href"))
+            href = link.get("href")
+
+            if not href:
+                continue
+
+            url = self.absolutize(base_url, href)
 
             text_blob = clean_text(link.parent.get_text(" ", strip=True))
-
             title = clean_text(link.get_text())
 
             listings.append(
